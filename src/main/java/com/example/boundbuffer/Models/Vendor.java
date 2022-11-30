d6eac77b19e85cdd29ae8a5cd2e96593688f6c66
MohammedZakarya <105216292+MohammedZakarya@users.noreply.github.com>
2022-11-30 23:08:54 +0200

    implemented insert method in Vendor class implemented BoundBuffer class

--- src/main/java/com/example/boundbuffer/Models/Vendor.java
+++ src/main/java/com/example/boundbuffer/Models/Vendor.java
@@ -1,11 +1,23 @@
 package com.example.boundbuffer.Models;
-import com.example.boundbuffer.Models.Supermarket;
+import com.example.boundbuffer.Models.*;
+import java.util.concurrent.Semaphore;
 
 public class Vendor {
     Vendor(){
 
     }
-    public void Insert(Object item, Supermarket supermarket ){
+    public synchronized void Insert(Object item, Supermarket supermarket ) throws InterruptedException {
+        supermarket.empty.acquire();
+        supermarket.mutex.acquire();
+        // add an item to the buffer
 
+        supermarket.isFull();
+        supermarket.stock[supermarket.in] = item;
+
+        supermarket.in = (supermarket.in + 1) % supermarket.size;
+
+        supermarket.mutex.release();
+        supermarket.full.release();
+        System.out.println("I'm Vendor ");
     }
 }
