243a5bf0a2a3b661923964a73c91c13850df758b
tahasaeed200 <tahasaeed749@gmail.com>
2022-12-02 16:30:01 +0200

    created main page view

--- src/main/java/com/example/boundbuffer/Controllers/HelloController.java
+++ src/main/java/com/example/boundbuffer/Controllers/HelloController.java
@@ -1,14 +1,16 @@
 package com.example.boundbuffer.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    Button sellid,buyid;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+
+
+
+
 }
