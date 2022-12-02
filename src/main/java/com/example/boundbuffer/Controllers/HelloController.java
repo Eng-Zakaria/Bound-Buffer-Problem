13fe55ae81594d006a43df06bf973e330dfe2fb1
FathyAhmed20 <fa946918@gmail.com>
2022-12-02 19:48:34 +0200

    customer view

--- src/main/java/com/example/boundbuffer/Controllers/HelloController.java
+++ src/main/java/com/example/boundbuffer/Controllers/HelloController.java
@@ -1,16 +1,30 @@
 package com.example.boundbuffer.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
     @FXML
     Button sellid,buyid;
 
 
-
-
+//    public void SwitchToScene2(ActionEvent event) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
 
 
 }
--- src/main/java/com/example/boundbuffer/HelloApplication.java
+++ src/main/java/com/example/boundbuffer/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.boundbuffer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,6 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+//  تبع كود الربط
+//        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 550);
         stage.setTitle("Hello!");
